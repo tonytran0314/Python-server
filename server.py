e088d176cdd1c2d556ffc254e6cbfdc325d72e9f
tonytran0314 <tonytran0314@gmail.com>
2021-08-19 11:50:50 -0400

    add details route

--- server.py
+++ server.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route("details")
+def detls():
+    return "This is details page"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
