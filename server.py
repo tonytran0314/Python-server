79709354e10532a4e05577efb08a6ba153aab2e3
tonytran0314 <tonytran0314@gmail.com>
2021-08-19 11:56:32 -0400

    add 2 html files | edit routes

--- server.py
+++ server.py
@@ -1,16 +1,16 @@
 #Server file
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return render_template("index.html")
 
 @app.route("/details")
 def detls():
-    return "This is details page"
+    return render_template("details.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80, debug=True)
